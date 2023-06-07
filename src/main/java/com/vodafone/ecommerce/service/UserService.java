3ea4740ad98a0d66b09975a796e2d50b9cff9e9e
Mohaned Ashraf <businessmohaned@gmail.com>
2023-06-07 16:05:57 +0200

    Feature: Add UserService.java Method Signature

---
+++ src/main/java/com/vodafone/ecommerce/service/UserService.java
@@ -0,0 +1,37 @@
+package com.vodafone.ecommerce.service;
+
+public class UserService {
+
+    // Create User
+    public User createUser(User user)
+    {
+        return null;
+    }
+    // Update User
+    public User updateUser(User user)
+    {
+        return null;
+    }
+    // Delete User
+    public User deleteUser(User user)
+    {
+        return null;
+    }
+
+    // Update Password
+    public User updatePassword(String oldPassword, String newPassword)
+    {
+        return null;
+    }
+
+    // Update Email
+    public User updateEmail(String newEmail)
+    {
+        return null;
+    }
+    // Update Full Name
+    public User updateFullName(String fullName)
+    {
+        return null;
+    }
+}
