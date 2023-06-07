225d29acc53a9b2e7c1b5d32367c703ecad19c10
Ahmed Ossama <ahmed.o.ashour1@gmail.com>
2023-06-07 15:22:45 +0300

    Feature:Create product entity

--- src/main/java/com/vodafone/ecommerce/model/Product.java
+++ src/main/java/com/vodafone/ecommerce/model/Product.java
@@ -1,15 +1,23 @@
 package com.vodafone.ecommerce.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
-@Table(name = "product")
+@Table(name = "Product")
+@AllArgsConstructor
 public class Product
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    private String name;
+    private String category; //enum or db table?
+    private Double price;
+    private Integer stock;
+    private Double rating;
+    private String image;
 }
