ddd3ae72742c9e6248b89960f266c4c2c75d24f2
Ahmed Ossama <ahmed.o.ashour1@gmail.com>
2023-06-15 15:41:38 +0300

    Refactor: Handle unkown exceptions

--- src/main/java/com/vodafone/ecommerce/exception/MVCExceptionHandler.java
+++ src/main/java/com/vodafone/ecommerce/exception/MVCExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.vodafone.ecommerce.exception;
 
 import com.vodafone.ecommerce.model.ErrorDetails;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
